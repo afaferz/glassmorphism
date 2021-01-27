2ce8f4ad66a9d114cd06a9bf344ce4b4c4904216
afaferz <afa00.papa@gmail.com>
2021-01-27 15:32:01 -0300

    Init

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/glassmorphism/'
+    : 'dist',
     filename: 'build.js'
   },
   module: {
